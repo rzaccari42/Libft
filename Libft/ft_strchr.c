c7e55461136aa65763ade1b68c4f2f0caf845b32
Raphael <rzaccari@student.42lausanne.ch>
2021-11-09 23:26:53 +0100

    Correction ft_strchr
    Removed main and includes

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,13 +6,11 @@
 /*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 15:07:31 by rzaccari          #+#    #+#             */
-/*   Updated: 2021/11/09 23:17:07 by rzaccari         ###   ########.fr       */
+/*   Updated: 2021/11/09 23:25:06 by rzaccari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include<libft.h>
-#include<string.h>
-#include<stdio.h>
+#include<libft.h>
 
 char	*ft_strchr(const char *str, int c)
 {
@@ -28,14 +26,4 @@ char	*ft_strchr(const char *str, int c)
 	if (c == '\0')
 		return ((char*)str + i);
 	return (NULL);
-}
-
-int	main(void)
-{
-	char	str[] = "Raph est def";
-	int		c = 0;
-
-	printf("%s\n", strchr(str, c));
-
-	printf("%s\n", ft_strchr(str, c));
 }
