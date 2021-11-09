77ef45ddc3c39721ba84a3be45e5bd7d3d5b3bfa
Raphael <rzaccari@student.42lausanne.ch>
2021-11-09 23:23:07 +0100

    ft_strchr finished

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 15:07:31 by rzaccari          #+#    #+#             */
-/*   Updated: 2021/11/08 16:42:35 by rzaccari         ###   ########.fr       */
+/*   Updated: 2021/11/09 23:17:07 by rzaccari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,23 @@ char	*ft_strchr(const char *str, int c)
 	int		i;
 
 	i = 0;
-	while (str[i] != c)
+	while (str[i])
 	{
-		if (!str[i])
-		{
-			return (0);
-		}
+		if (str[i] == c)
+			return ((char*)str + i);
 		i++;
 	}
-	return ((char*)str + i);
+	if (c == '\0')
+		return ((char*)str + i);
+	return (NULL);
 }
 
 int	main(void)
 {
-	char	str[] = "abcdef";
-	int		c = 'o';
+	char	str[] = "Raph est def";
+	int		c = 0;
+
+	printf("%s\n", strchr(str, c));
 
 	printf("%s\n", ft_strchr(str, c));
-	printf("%s", strchr(str, c));
 }
