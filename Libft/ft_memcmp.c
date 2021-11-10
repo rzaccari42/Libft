314569905f289e924de08698b4065e4624b60b2d
Raphael <rzaccari@student.42lausanne.ch>
2021-11-10 23:02:33 +0100

    ft_memcmp finished

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,13 +6,11 @@
 /*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 19:43:36 by rzaccari          #+#    #+#             */
-/*   Updated: 2021/11/10 21:00:06 by rzaccari         ###   ########.fr       */
+/*   Updated: 2021/11/10 23:02:08 by rzaccari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include<string.h>
-#include<stdio.h>
+#include "libft.h"
 
 int	ft_memcmp(const void *str1, const void *str2, size_t len)
 {
@@ -24,14 +22,4 @@ int	ft_memcmp(const void *str1, const void *str2, size_t len)
 	str2++;
 	}
 	return (0);
-}
-
-int main(void)
-{
-	const char s1[] = "AlloPolrd";
-	const char s2[] = "AploPolrd";
-	size_t len = 2;
-
-	printf("%d\n", memcmp(s1, s2, len));
-	printf("%d", ft_memcmp(s1, s2, len));
 }
