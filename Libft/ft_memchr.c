d56450cea87174277c6c843debded15f62d612c0
Raphael <rzaccari@student.42lausanne.ch>
2021-11-10 19:11:43 +0100

    ft_memchr finished

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,13 +6,11 @@
 /*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:10:11 by rzaccari          #+#    #+#             */
-/*   Updated: 2021/11/10 17:09:02 by rzaccari         ###   ########.fr       */
+/*   Updated: 2021/11/10 19:09:22 by rzaccari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include<string.h>
-#include<stdio.h>
+#include "libft.h"
 
 void	*ft_memchr(const void *str, int c, size_t len)
 {
@@ -20,19 +18,7 @@ void	*ft_memchr(const void *str, int c, size_t len)
 	{
 		if (*(unsigned char*)str == (unsigned char)c)
 			return ((void*)str);
-		*(unsigned char*)str++;
+		str++;
 	}
 	return (NULL);
-}
-
-int	main()
-{
-	char	str[] = "Hello World";
-	int		c = 'x';
-	size_t	len = 10;
-	char *ptr1 = memchr(str, c, len);
-	char *ptr2 = ft_memchr(str, c, len);
-
-	printf("%s\n", ptr1);
-	printf("%s", ptr2);
 }
