d90aef91f08bcd5dadd3b921cdb1f10a1dad5259
Raphael <rzaccari@student.42lauanne.ch>
2021-11-18 17:36:35 +0100

    Correcting ft_strlcpy and ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rzaccari <rzaccari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 13:22:03 by rzaccari          #+#    #+#             */
-/*   Updated: 2021/11/18 17:18:25 by rzaccari         ###   ########.fr       */
+/*   Updated: 2021/11/18 17:31:02 by rzaccari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	ft_strncmp(const char *str1, const char *str2, size_t len)
 	{
 		if (*str1 != *str2)
 			return (*str1 - *str2);
-		*str1++;
-		*str2++;
+		str1++;
+		str2++;
 	}
 	return (0);
 }
