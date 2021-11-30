98e015281ced821580d0cccac8e56e1874275b64
Raphael Zaccaria <raphaelzaccaria@outlook.com>
2021-11-30 13:45:17 +0100

    ft_putstr_fd
    Created and finished

---
+++ Libft/ft_putstr_fd.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 12:43:07 by rzaccari          #+#    #+#             */
+/*   Updated: 2021/11/30 13:43:59 by rzaccari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	int	len;
+
+	if (!str)
+		return (NULL);
+	len = ft_strlen(str);
+	write(fd, str, len);
+}
