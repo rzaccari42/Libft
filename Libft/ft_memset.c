28dd7b2766668647279cd5a51486a4f8043a80a3
Raphael Zaccaria <raphaelzaccaria@outlook.com>
2021-12-03 17:25:30 +0100

    ft_memset correction

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,15 +6,20 @@
 /*   By: rzaccari <rzaccari@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 16:17:44 by rzaccari          #+#    #+#             */
-/*   Updated: 2021/11/04 01:25:25 by rzaccari         ###   ########.fr       */
+/*   Updated: 2021/12/03 17:25:09 by rzaccari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<libft.h>
+#include "libft.h"
 
 void	*ft_memset(void *ptr, int val, size_t len)
 {
+	unsigned char	*str;
+	int				i;
+
+	str = ptr;
+	i = 0;
 	while (len--)
-		*(char*)ptr++ = val;
+		str[i++] = val;
 	return (ptr);
 }
